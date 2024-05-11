c9f4bbbd7ae3477775d4a9c4cf2c329bfc4d97c3
vikas-pai <paivikas1402@gmail.com>
2024-05-11 23:42:58 +0530

    Implemented main class for scheduling algorithms comparison

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cloud-task-scheduling-algorithms
---
+++ src/Scheduler_Comparison.java
@@ -0,0 +1,35 @@
+import FCFS.FCFS_Scheduler;
+import RoundRobin.RoundRobin_Scheduler;
+import SJF.SJF_Scheduler;
+import utils.Commons;
+import utils.GenerateLengthMatrix;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+public class Scheduler_Comparison {
+    public static void main(String[] args) throws InterruptedException {
+        SortedMap<Double, String> map = new TreeMap<>();
+        new GenerateLengthMatrix();
+        Commons.lengthMatrix = GenerateLengthMatrix.getlengthMatrix();
+        map.put(FCFS_Scheduler.main(args), "First Come-First Serve");
+        System.out.println("===========================================");
+        TimeUnit.SECONDS.sleep(1);
+        map.put(SJF_Scheduler.main(args), "Shortest Job First");
+        System.out.println("===========================================");
+        TimeUnit.SECONDS.sleep(1);
+        map.put(RoundRobin_Scheduler.main(args), "Round Robin");
+        System.out.println("===========================================");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("===========================================");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("===========================================");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Sorted list of algorithms (criteria: earliest finish time)");
+        for(double i: map.keySet()){
+            System.out.printf("%s: %.2f%n", map.get(i), i);
+        }
+        System.out.println("===========================================");
+    }
+}
